238309a56eeb4855bfb39e9682758e068c79902d
Jinseop Lee <js428118@gmail.com>
2019-03-03 22:32:19 +0900

    울타리, 알파카 생성 및 BGM을 추가한다.
    알파카가 등장하는 울타리와 알파카의 등장 이벤트를 생성하고 BGM(U can't touch this)를 추가한다.
    
    알파카는 Map 객체로 관리된다. element가 key가 되고 row number, column number, isShow로 프로퍼티가 구성된다. Map으로 관리하는 이유는 현재 추가되어있는 알파카 클릭 이벤트와 앞으로 추가될 여러 기능들에서 element와 알파카의 상태를 쉽게 관리하기 위해서 Map 객체로 관리한다.
    
    BGM은 Youtube에서 iframe소스를 가져와 사용하였다. 원래 같은 구간을 계속 반복재생하려 했지만 iframe의 너비와 높이를 0으로 설정하면 한 번 밖에 재생되지 않는다. 그래서 러닝타임이 긴 트랙을 가져와서 게임이 플레이되는 동안에만 재생시킬것이다. (게임을 한번 플레이하는데 최대 시간은 60초로 정할 예정)
    
    앞으로 추가될 내용
    1. 게임 시간 60초 내에 얼마나 많은 알파카를 잡는지 카운팅
    2. 게임 시간이 지날수록 난이도 상승
        - 알파카가 등장했다가 사라지는 속도를 더 빠르게 변경
        - 알파카와 함정을 동시에 등장시키는 기능 추가
    3. 게임 난이도 설정 기능을 추가
        - 난이도 1 : 울타리 5개(1줄)
        - 난이도 2 : 울타리 10개(2줄)
        - 난이도 3 : 울타리 15개(3줄)

---
+++ 알파카를잡아라/index.js
@@ -0,0 +1,96 @@
+/**
+ * 알파카 스펙에 대한 객체
+ */
+const ALPACA = {
+    width: 150,
+    height: 150,
+    show: "./image/ShowAlpaca.png",
+    hide: "./image/HideAlpaca.png"
+};
+Object.freeze(ALPACA);
+
+/**
+ * 게임 설정에 대한 객체
+ */
+const GameSet = {
+    TIME_INTERVAL: 1500,
+    SHOW_ALPACA: 1000
+}
+Object.freeze(GameSet);
+
+/**
+ * 점수 컨트롤 함수
+ */
+const controlScore = () => {
+
+}
+
+/**
+ * 알파카 생성 함수
+ * @param {*} level 레벨이 높아질수록 알파카가 많아짐(기본 5개)
+ */
+const makeAlpaca = (level) => {
+    // 알파카의 상태를 기록한다.
+    const alpacaMap = new Map();
+
+    // 가로 5마리, 세로 n(1~3)줄을 생성하고 초기화한다.
+    for (let row = 0; row < level; row++) {
+        for (let col = 0; col < 5; col++) {
+
+            // 알파카 생성
+            const element = document.createElement("div");
+            element.style.width = `${ALPACA.width}px`;
+            element.style.height = `${ALPACA.height}px`;
+            element.style.background = `url("${ALPACA.hide}") no-repeat`;
+            console.log("===========")
+            console.log(element);
+
+            // 알파카를 div#map에 배치한다.
+            document.getElementById("map").appendChild(element);
+
+            alpacaMap.set(element, {
+                row, col, isShow: false
+            });
+
+            // 알파카 클릭 이벤트 생성
+            element.onclick = (e) => {
+                const element = e.currentTarget;
+                const alpaca = alpacaMap.get(element);
+
+                if(alpaca.isShow) {
+                    console.log("getcha!")
+                    element.style.background = `url("${ALPACA.hide}") no-repeat`;
+                    alpaca.isShow = false;
+                }
+            }
+        }
+    }
+
+    return alpacaMap;
+};
+
+/**
+ * 알파카를 랜덤으로 등장시키는 함수
+ * @param {*} alpacas 알파카 Map 객체
+ */
+const alpacaRandomShow = (alpacas) => {
+    const elements = alpacas.keys();
+    const elementsArr = Array.from(elements);
+    const who = Math.floor(Math.random() * alpacas.size);
+    
+    elementsArr[who].style.background = `url(${ALPACA.show}) no-repeat`;
+    alpacas.get(elementsArr[who]).isShow = true;
+
+    setTimeout(() => {
+        elementsArr[who].style.background = `url(${ALPACA.hide}) no-repeat`;
+        alpacas.get(elementsArr[who]).isShow = false;
+    }, GameSet.SHOW_ALPACA);
+};
+
+window.onload = () => {
+    // 알파카 생성
+    const alpacas = makeAlpaca(3);
+    var timer = setInterval(() => {
+        alpacaRandomShow(alpacas);
+    }, GameSet.TIME_INTERVAL) ;
+};
