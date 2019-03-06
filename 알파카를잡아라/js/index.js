5f17386e630c697a48ccd4c411540d27137c5c9c
Jinseop Lee <js428118@gmail.com>
2019-03-06 21:38:54 +0900

    타이머 카운트, 점수 가산 기능을 추가한다.
    게임 진행에 대한 타이머 카운트 기능과 점수 가산 기능을 추가한다.
    
    START 버튼에 게임을 초기화하고 시작하는 기능을 추가하고 게임이 진행되는동안 스코어를 가산하는 기능을 추가한다. 추가로 게임 시작 전 가이드라인과 게임이 종료된 후 가이드라인을 추가한다.
    
    추가할 내용
    0. 난이도는 기본 EASY로 설정 & 게임 도중에는 버튼 조작 불가능하도록 설정
    1. 게임 종료 버튼 추가
    2. 난이도에 따른 알파카 울타리 갯수, 알파카가 등장해있는 시간, 스코어 가산 점수 처리
    3. 카운트 다운의 진행도에 따라서 게임 난이도 조정(알파카가 등장해있는 시간, 스코어 가산 점수)

--- 알파카를잡아라/js/index.js
+++ 알파카를잡아라/js/index.js
@@ -12,21 +12,49 @@ const ALPACA = {
 Object.freeze(ALPACA);
 
 /**
- * 게임 설정에 대한 객체
+ * 게임 기능 요소에 대한 객체
  */
-const GameSet = {
-    TIME_INTERVAL: 1500,
-    SHOW_ALPACA: 1000,
-    PLAY_TIME: 60
-}
-Object.freeze(GameSet);
+const Game = {
+    set: {  // 게임 기본 설정 프로퍼티
+        TIME_INTERVAL: 1500,
+        SHOW_ALPACA: 1000,
+        PLAY_TIME: 60
+    },
+    steps: { // 게임 난이도 프로퍼티
+        EASY: {},
+        MEDIUM: {},
+        HARD: {}
+    },
+    panel: { // 게임 컨트롤에 관련된 DOM(난이도, 시작, 타이머, 점수)
+        start: document.getElementById("gameStart"),
+        time: document.getElementById("time"),
+        score: document.getElementById("score")
+    },
+    map: document.getElementById("map") // 게임 Map
+};
+Object.freeze(Game);
+
+/**
+ * node의 내부에 있는 node들을 모두 지우는 함수
+ * @param {*} node 내부를 청소할 노드
+ */
+const clearChildren = (node) => {
+    
+    if (!node) return;
+
+    while (node.childElementCount > 0) {
+        const child = node.childNodes.item(0);
+        node.removeChild(child);
+    }
+};
 
 /**
  * 점수 컨트롤 함수
  */
-const controlScore = () => {
-
-}
+const controlScore = (point) => {
+    const score = parseInt(Game.panel.score.innerHTML);
+    Game.panel.score.innerHTML = score + point;
+};
 
 /**
  * 알파카 생성 함수
@@ -36,7 +64,10 @@ const makeAlpaca = (level) => {
     // 알파카의 상태를 기록한다.
     const alpacaMap = new Map();
 
-    // 가로 5마리, 세로 n(1~3)줄을 생성하고 초기화한다.
+    // map 내부의 노드를 모두 제거한다.
+    clearChildren(Game.map);
+
+    // 가로 5마리, 세로 n(1~3)줄을 생성하고 초기화 한다.
     for (let row = 0; row < level; row++) {
         for (let col = 0; col < 5; col++) {
 
@@ -48,23 +79,25 @@ const makeAlpaca = (level) => {
             element.style.cursor = `pointer`;
 
             // 알파카를 div#map에 배치한다.
-            document.getElementById("map").appendChild(element);
+            Game.map.appendChild(element);
 
             alpacaMap.set(element, {
                 row, col, isShow: false
             });
 
             // 알파카 클릭 이벤트 생성
-            element.onclick = (e) => {
+            element.addEventListener("click", (e) => {
                 const element = e.currentTarget;
                 const alpaca = alpacaMap.get(element);
 
                 if(alpaca.isShow) {
-                    console.log("getcha!")
                     element.style.background = `url("${ALPACA.hide}") no-repeat`;
                     alpaca.isShow = false;
+
+                    // 점수 추가
+                    controlScore(500);
                 }
-            }
+            });
         }
     }
 
@@ -75,7 +108,7 @@ const makeAlpaca = (level) => {
  * 알파카를 랜덤으로 등장시키는 함수
  * @param {*} alpacas 알파카 Map 객체
  */
-const alpacaRandomShow = (alpacas) => {
+const showAlpacaRandomly = (alpacas) => {
     const elements = alpacas.keys();
     const elementsArr = Array.from(elements);
     const who = Math.floor(Math.random() * alpacas.size);
@@ -86,13 +119,62 @@ const alpacaRandomShow = (alpacas) => {
     setTimeout(() => {
         elementsArr[who].style.background = `url(${ALPACA.hide}) no-repeat`;
         alpacas.get(elementsArr[who]).isShow = false;
-    }, GameSet.SHOW_ALPACA);
+    }, Game.set.SHOW_ALPACA);
+};
+
+/**
+ * 게임진행에 대한 안내 메세지를 p 태그로 생성하는 함수
+ * @param {*} message 게임 진행에 대한 안내 메세지
+ */
+const makeMessageElement = (message) => {
+    const element = document.createElement("p");
+    element.innerText = message;
+    element.style.fontSize = "40px";
+    element.style.fontWeight = 700;
+    element.style.padding = "80px 0";
+    return element;
+};
+
+/**
+ * 게임 진행 관련 버튼 세팅 및 시간 초와 점수 관련 이벤트 초기화 함수
+ */
+const initPanel = () => {
+
+    Game.map.appendChild(makeMessageElement("PRESS THE START BUTTON AT THE TOP"));
+
+    // 게임 시작 버튼
+    Game.panel.start.addEventListener("click", () => {
+
+        // 게심 시작시 timer를 PLAY_TIME으로 초기화, 스코어를 0으로 초기화한다.
+        Game.panel.time.innerHTML = `${Game.set.PLAY_TIME}S`;
+        Game.panel.score.innerHTML = 0;
+
+        // 알파카 생성 및 Interval 실행
+        const alpacas = makeAlpaca(3);
+        const alpacaInterval = setInterval(() => {
+            showAlpacaRandomly(alpacas);
+        }, Game.set.TIME_INTERVAL);
+
+        // 게임 타이머 설정
+        let s = Game.set.PLAY_TIME;
+        const gameCountdown = setInterval(() => {
+
+            // 타이머 카운트 다운
+            Game.panel.time.innerHTML = `${--s}s`;
+            
+            // 카운트 다운이 끝나면 알파카 Interval과 게임 타이머 clear
+            if (s < 1) {
+                clearInterval(alpacaInterval);
+                clearInterval(gameCountdown);
+                
+                // 게임 맵 초기화 후 게임 오버 메세지와 스코어 점수 게시
+                clearChildren(Game.map);
+                Game.map.appendChild(makeMessageElement(`GAME OVER!\nSCORE: ${Game.panel.score.innerHTML}`));
+            }
+        }, 1000);
+    });
 };
 
 window.onload = () => {
-    // 알파카 생성
-    const alpacas = makeAlpaca(3);
-    const timer = setInterval(() => {
-        alpacaRandomShow(alpacas);
-    }, GameSet.TIME_INTERVAL) ;
+    initPanel();
 };
